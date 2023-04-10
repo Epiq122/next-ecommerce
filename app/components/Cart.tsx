c8c890fc5753b479dff86e74c08da58867417758
Robert Gleason <l2ob122@gmail.com>
2023-04-09 21:20:08 -0700

    added roboto and lobster_two to layout page

--- app/components/Cart.tsx
+++ app/components/Cart.tsx
@@ -1,30 +1,30 @@
-"use client";
+'use client';
 
-import Image from "next/image";
-import { useCartStore } from "@/store";
-import formatPrice from "@/util/PriceFormat";
-import { IoAddCircle } from "react-icons/all";
-import { IoRemoveCircle } from "react-icons/all";
-import purchase from "@/public/purchase.png";
+import Image from 'next/image';
+import { useCartStore } from '@/store';
+import formatPrice from '@/util/PriceFormat';
+import { IoAddCircle } from 'react-icons/io5';
+import { IoRemoveCircle } from 'react-icons/io5';
+import purchase from '@/public/purchase.png';
 
 export default function Cart() {
   const cartStore = useCartStore();
   return (
     <div
       onClick={() => cartStore.toggleCart()}
-      className="fixed w-full h-screen left-0 top-0 bg-black/25"
+      className='fixed w-full h-screen left-0 top-0 bg-black/25'
     >
       <div
         onClick={(e) => e.stopPropagation()}
-        className="bg-white absolute right-0 top-0 w-1/4 h-screen p-12 overflow-y-scroll text-gray-700"
+        className='bg-white absolute right-0 top-0 w-1/4 h-screen p-12 overflow-y-scroll text-gray-700'
       >
         <h1>Shopping list💵</h1>
         {cartStore.cart.map((item) => (
-          <div className="flex py-4 gap-4">
+          <div className='flex py-4 gap-4'>
             <Image src={item.image} alt={item.name} width={120} height={120} />
             <div>
               <h2>{item.name}</h2>
-              <div className="flex gap-2 text-lg">
+              <div className='flex gap-2 text-lg'>
                 <button
                   onClick={() =>
                     cartStore.removeProduct({
@@ -53,21 +53,21 @@ export default function Cart() {
                 </button>
               </div>
               <h2>Quantity: {item.quantity}</h2>
-              <p className="text-sm">
+              <p className='text-sm'>
                 {item.unit_amount && formatPrice(item.unit_amount)}
               </p>
             </div>
           </div>
         ))}
         {cartStore.cart.length > 0 && (
-          <button className="py-2 mt-4 bg-teal-700 w-full rounded-md text-white ">
+          <button className='py-2 mt-4 bg-teal-700 w-full rounded-md text-white '>
             Checkout
           </button>
         )}
         {!cartStore.cart.length && (
-          <div className="flex flex-col items-center gap-12 text-2xl font-medium pt-56 opacity-75">
+          <div className='flex flex-col items-center gap-12 text-2xl font-medium pt-56 opacity-75'>
             <h1>Cart is Empty...</h1>
-            <Image src={purchase} alt="empty cart" width={200} height={200} />
+            <Image src={purchase} alt='empty cart' width={200} height={200} />
           </div>
         )}
       </div>
